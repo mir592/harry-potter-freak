8bb16d1617c560c46ae8ef46c0ada0cdb4de55b9
mvalero <miriam5592@gmail.com>
2022-12-19 14:55:08 +0100

    feat: rename component

---
+++ components/CharacterCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img v-if="src" :src="src" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">{{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterCard',
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
